4f10319b0e167f07dc5af296c2c32d02e55ce8c8
nguyenhien161 <lukenguyen161@gmail.com>
2022-11-02 23:57:10 +0700

    upload code

--- src/pages/pageAdmin/Admin.jsx
+++ src/pages/pageAdmin/Admin.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-  } from '@ant-design/icons';
-  import { Breadcrumb, Layout, Menu } from 'antd';
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu } from "antd";
+import Film from "./Film";
 
-    const { Content, Sider } = Layout;
+const { Content, Sider } = Layout;
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -18,79 +19,63 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
   ]),
-  getItem('Film', '6', <FileOutlined />),
+  getItem("Film", "6", <FileOutlined />),
 ];
 const Admin = () => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout
-    style={{
-      minHeight: '100vh',
-    }}
-  >
-    <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-      <div className="logo" />
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-    </Sider>
-    <Layout className="site-layout">
-      <Content
-        style={{
-          margin: '0 16px',
-        }}
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
       >
-        <Breadcrumb
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Content
           style={{
-            margin: '16px 0',
+            margin: "0 16px",
           }}
         >
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-        <div
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            minHeight: 360,
-          }}
-        >
-          Bill is a cat.
-        </div>
-      </Content>
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Film />
+          </div>
+        </Content>
+      </Layout>
     </Layout>
-    <Layout className="site-layout">
-      <Content
-        style={{
-          margin: '0 16px',
-        }}
-      >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
-          }}
-        >
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-        <div
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            minHeight: 360,
-          }}
-        >
-          Bill is a cat.
-        </div>
-      </Content>
-    </Layout>
-  </Layout>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
---
+++ src/pages/pageAdmin/Film.jsx
@@ -0,0 +1,140 @@
+import React, { Fragment, useEffect } from "react";
+import { Button, Table } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getMovieList, useQuanLyPhim } from "../../storeToolkit/quanLyPhim";
+import { NavLink } from "react-router-dom";
+
+const { Search } = Input;
+
+const Film = () => {
+  const { movieList } = useQuanLyPhim();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovieList());
+  }, []);
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: "#1890ff",
+      }}
+    />
+  );
+  const onSearch = (value) => console.log(value);
+  const columns = [
+    {
+      title: "Mã phim",
+      dataIndex: "maPhim",
+      value: (text, Object) => {
+        return <span>{text}</span>;
+      },
+      sorter: (a, b) => a.maPhim - b.maPhim,
+      sortDirections: ["descend", "ascend"],
+      width: "15%",
+    },
+    {
+      title: "Hình ảnh",
+      dataIndex: "hinhAnh",
+      render: (text, film, index) => {
+        return (
+          <Fragment>
+            <img
+              src={film.hinhAnh}
+              alt={film.tenPhim}
+              width={50}
+              height={50}
+              ỏnError={(e) => {
+                e.target.onError = null;
+                e.target.src = `https://picsum.photos/id/${index}/50/50`;
+              }}
+            />
+          </Fragment>
+        );
+      },
+      width: "15%",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Tên phim",
+      dataIndex: "tenPhim",
+      sorter: (a, b) => {
+        let tenPhimA = a.tenPhim.toLoverCase().trim();
+        let tenPhimB = b.tenPhim.toLoverCase().trim();
+        if (tenPhimA > tenPhimB) {
+          return 1;
+        }
+        return -1;
+      },
+      sortDirections: ["descend", "ascend"],
+      width: "20%",
+    },
+    {
+      title: "Mô tả",
+      dataIndex: "moTa",
+      //   sorter: (a, b) => {
+      //     let moTaA = a.moTa.toLoverCase().trim();
+      //     let moTaB = b.moTa.toLoverCase().trim();
+      //     if (moTaA > moTaB) {
+      //       return 1;
+      //     }
+      //     return -1;
+      //   },
+      render: (text, film) => {
+        return (
+          <Fragment>
+            {film.moTa.length > 50
+              ? film.moTa.substr(0, 50) + " ..."
+              : film.moTa}
+          </Fragment>
+        );
+      },
+      width: "20%",
+    },
+    {
+      title: "Hành động",
+      dataIndex: "hanhDong",
+      //   sorter: (a, b) => {
+      //     let moTaA = a.moTa.toLoverCase().trim();
+      //     let moTaB = b.moTa.toLoverCase().trim();
+      //     if (moTaA > moTaB) {
+      //       return 1;
+      //     }
+      //     return -1;
+      //   },
+      render: (text, film) => {
+        return (
+          <Fragment>
+            <NavLink className="bg-dark text-white" to="/"></NavLink>
+          </Fragment>
+        );
+      },
+      width: "20%",
+    },
+  ];
+  const data = movieList;
+
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
+  return (
+    <div>
+      <h3 className="text-30">Quản lý phim</h3>
+      <Button className="mb-3">Thêm phim</Button>
+      <Search
+        className="mb-4"
+        placeholder="input search text"
+        enterButton
+        size="large"
+        onSearch={onSearch}
+      />
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </div>
+  );
+};
+
+export default Film;
